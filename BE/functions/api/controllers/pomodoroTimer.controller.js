e4b28dd94bc958f64e6e00b1ccb0b34c2c4dc0b4
Seaw Teng <seawteng@gmail.com>
2024-06-13 10:40:22 +0800

    change promodo post to get

--- BE/functions/api/controllers/pomodoroTimer.controller.js
+++ BE/functions/api/controllers/pomodoroTimer.controller.js
@@ -1,35 +1,32 @@
-import { pomodoroTimerRepository } from "../../repositories/index.js";
+import {pomodoroTimerRepository} from "../../repositories/index.js";
 import jwt from "jsonwebtoken";
 
 /**
  * TodoTaskController
 */
-class pomodoroTimerController {
-    /**
+class PomodoroTimerController {
+  /**
      *  @param {any} req
     */
-    async getAllByUser(req) {
-        return await pomodoroTimerRepository.getAllByUser(req);
-    }
+  async getAllByUser(req) {
+    return await pomodoroTimerRepository.getAllByUser(req);
+  }
 
-
-    /**
+  /**
      *  @param {any} req
      *  @param {pomodoroTimer} pomoSess
     */
-    async create(req, pomoSess) {
-        const token = req.headers["authorization"];
-        const userId = jwt.decode(token, process.env.JWT_SECRET).id;
+  async create(req, pomoSess) {
+    const token = req.headers["authorization"];
+    const userId = jwt.decode(token, process.env.JWT_SECRET).id;
 
-        pomoSess.isDeleted = false;
-        const userDocRef = await pomodoroTimerRepository.db
-            .doc(`users/${userId}`);
-        pomoSess.userId = userDocRef;
+    pomoSess.isDeleted = false;
+    const userDocRef = await pomodoroTimerRepository.db
+        .doc(`users/${userId}`);
+    pomoSess.userId = userDocRef;
 
-        return await pomodoroTimerRepository.add(req, pomoSess);
-    }
-
-
+    return await pomodoroTimerRepository.add(req, pomoSess);
+  }
 }
 
-export default new pomodoroTimerController();
+export default new PomodoroTimerController();
