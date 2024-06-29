c66c2317ed09209d0480efaceae8d3ac05a69bbc
scoopydoooKing <148352740+scoopydoooKing@users.noreply.github.com>
2024-06-30 05:26:07 +0800

    include filter for dashboard but some not working and made changes to pomo

--- FE/src/app/components/dashboard/dashboard.component.ts
+++ FE/src/app/components/dashboard/dashboard.component.ts
@@ -5,21 +5,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { ServicesService } from '../../services/services.service';
 import { ToastrService } from 'ngx-toastr';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { start } from 'repl';
+import { NgxLoadingModule } from 'ngx-loading';
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, NgxLoadingModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+
   //charts 
   title = 'ng-chart';
   cTaskchart: any = [];
-  TaskStatuschart: any = [];
+  TaskStatuschart: any = null;
   eachTaskStackChart: any = [];
   pomoBar: any = [];
 
@@ -31,7 +35,6 @@ export class DashboardComponent {
   loading = false;
   selectedCategoryId: any = null;
   selectedTask: any = null;
-  selectedCategory: any = null;
   showCompleted: boolean = true;
 
   // upper dashboard 
@@ -39,6 +42,10 @@ export class DashboardComponent {
   completedToday = 0;
   overdueTasks = 0;
   productiveTime = 0;
+
+  //filter 
+  selectedPeriod = 7;
+  selectedCategory: any = null;
 
 
   constructor(
@@ -59,33 +66,7 @@ export class DashboardComponent {
   //   | | (_) | | |_| | (_) | | |___| | | | (_| | |  | |_\__ \
   //   |_|\___/  |____/ \___/   \____|_| |_|\__,_|_|   \__|___/
 
-  async numOfCompletedTaskBarChart() {
-    // allow filter and sat first show week by week then let user decide 
-    const completedTasks = this.taskList.filter((task: any) => task.status === 'completed');
-    const completedTasksByDate: { [date: string]: number } = {};
-    const todayDate = new Date().toISOString().split('T')[0];
-
-    completedTasks.forEach((task: any) => {
-      if (task.endDate) {
-        const date = new Date(task.endDate).toISOString().split('T')[0];
-        if (todayDate == date) {
-          this.completedToday += 1;
-        }
-        if (!completedTasksByDate[date]) {
-          completedTasksByDate[date] = 0;
-        }
-        completedTasksByDate[date]++;
-      }
-
-
-
-    });
-
-
-    const labels = Object.keys(completedTasksByDate);
-    const data = Object.values(completedTasksByDate);
-
-
+  async numOfCompletedTaskBarChart(labels: any, data: any) {
     this.cTaskchart = new Chart('completedTaskBarChart', {
       type: 'bar',
       data: {
@@ -110,43 +91,127 @@ export class DashboardComponent {
     });
   }
 
-  
 
-  async taskStatusPieChart() {
-    const pendingTasks = this.taskList.filter((task: any) => task.status === 'active').length;
-    const completedTasks = this.taskList.filter((task: any) => task.status === 'completed').length;
+  getFilteredData(period: number) {
+    const today = new Date();
+    let startDate: Date;
 
-    // should allow filter by category 
-    this.TaskStatuschart = new Chart('taskstatuspiechart', {
-      type: 'pie',
-      data: {
-        labels: ['Pending Tasks', 'Completed Tasks'],
-        datasets: [
-          {
-            label: 'Task Status',
-            data: [pendingTasks, completedTasks],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(75, 192, 192, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
+    switch (period) {
+      case 7:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        break;
+      case 31:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 31);
+        break;
+      case 93:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 93);
+        break;
+      default:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        break;
+    }
+
+
+    const filteredTasks = this.taskList.filter((task: any) => {
+      const taskDate = new Date(task.endDate);
+
+      const isCompleted = task.status === 'completed';
+      const isInRange = taskDate >= startDate && taskDate <= today;
+      // console.log('Is Completed:', isCompleted); 
+      // console.log('Is In Range:', isInRange); 
+      return isInRange && isCompleted;
+    });
+
+
+
+    const filteredTasksByDate: { [date: string]: number } = {};
+
+    filteredTasks.forEach((task: any) => {
+      const date = new Date(task.endDate).toISOString().split('T')[0];
+      if (!filteredTasksByDate[date]) {
+        filteredTasksByDate[date] = 0;
+      }
+      filteredTasksByDate[date]++;
+    });
+
+    const sortedEntries = Object.entries(filteredTasksByDate).sort((a, b) => {
+      return new Date(a[0]).getTime() - new Date(b[0]).getTime();
+    });
+
+
+
+    const labels = sortedEntries.map(entry => entry[0]);
+    const data = sortedEntries.map(entry => entry[1]);
+
+
+    return { labels, data };
+  }
+
+  async updateTaskChart() {
+
+    const filteredData = this.getFilteredData(this.selectedPeriod);
+
+
+    if (this.cTaskchart) {
+      this.cTaskchart.destroy();
+    }
+    this.numOfCompletedTaskBarChart(filteredData.labels, filteredData.data);
+  }
+
+  async updateCategory(selectedCategories: string) {
+    this.taskStatusPieChart(selectedCategories);
+  }
+
+  async taskStatusPieChart(selectedCategories?: string) {
+    const filteredTasks = this.taskList.filter((task: any) => {
+      const categoryId = task.categoryId._path.segments[1];
+      // console.log(`Task ID: ${task.id}, Category ID: ${categoryId}`);
+      return selectedCategories ? categoryId === selectedCategories : true;
+    });
+
+    const pendingTasks = filteredTasks.filter((task: any) => task.status === 'active').length;
+    const completedTasks = filteredTasks.filter((task: any) => task.status === 'completed').length;
+
+    // console.log(pendingTasks);
+    // console.log(completedTasks);
+
+    if (this.TaskStatuschart) {
+      this.TaskStatuschart.data.datasets[0].data = [pendingTasks, completedTasks];
+      this.TaskStatuschart.update();
+    } else {
+      this.TaskStatuschart = new Chart('taskstatuspiechart', {
+        type: 'pie',
+        data: {
+          labels: ['Pending Tasks', 'Completed Tasks'],
+          datasets: [
+            {
+              label: 'Task Status',
+              data: [pendingTasks, completedTasks],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
           },
         },
-      },
-    });
+      });
+    }
   }
+
+
 
 
   //   _                 _         _            _
@@ -179,7 +244,9 @@ export class DashboardComponent {
             .subscribe((res: any) => {
               this.loading = false;
               this.taskList = res;
-              this.numOfCompletedTaskBarChart();
+
+              const filteredData = this.getFilteredData(this.selectedPeriod);
+              this.numOfCompletedTaskBarChart(filteredData.labels, filteredData.data);
               this.taskStatusPieChart();
               this.findOverdueTask();
             });
@@ -194,8 +261,10 @@ export class DashboardComponent {
   }
 
   async findOverdueTask() {
-    const today = new Date();
-    const todayDate = today.toISOString().split('T')[0];
+    const today = new Date(new Date().getTime() + 8 * 60 * 60 * 1000)
+    const todayDate = today
+      .toISOString()
+      .split('T')[0];
     const pendingTasks = this.taskList.filter((task: any) => task.status === 'active');
     this.dueToday = 0;
     this.overdueTasks = 0;
@@ -221,78 +290,42 @@ export class DashboardComponent {
   // |_|   \___/|_| |_| |_|\___/   \____|_| |_|\__,_|_|   \__|___/
 
 
-  async eachTaskStackedChart() {
-    const completedTasks = this.taskList.filter((task: any) => task.status === 'completed');
-
-    const completedTasksByDate: { [date: string]: { [task: string]: number } } = {};
-
-    completedTasks.forEach((task: any) => {
-      if (task.endDateTime) {
-        const date = new Date(task.endDateTime).toISOString().split('T')[0];
-        if (!completedTasksByDate[date]) {
-          completedTasksByDate[date] = {};
-        }
-        if (!completedTasksByDate[date][task.task]) {
-          completedTasksByDate[date][task.task] = 0;
-        }
-        completedTasksByDate[date][task.task] += task.minutesTaken;
-      }
-    });
-
-    const labels = Object.keys(completedTasksByDate);
-    const taskNames: any[] = [...new Set(completedTasks.map((task: any) => task.task))];
-
-    const datasets = taskNames.map((taskName: any) => {
-      return {
-        label: taskName,
-        data: labels.map(date => completedTasksByDate[date][taskName] || 0),
-        borderWidth: 1,
-        backgroundColor: this.getRandomColor(),
-      };
-    });
-
-    this.eachTaskStackChart = new Chart('timeOnEachTaskStackChart', {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: datasets,
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          },
-        },
-        scales: {
-          x: {
-            stacked: true,
-          },
-          y: {
-            stacked: true,
-          },
-        },
-      },
-    });
-  }
 
   async totalPomoSessBarChart() {
 
     const pomoSessByDate: { [date: string]: number } = {};
-    const todayDate = new Date().toISOString().split('T')[0];
-
+    const todayDate = new Date(new Date().getTime() + 8 * 60 * 60 * 1000)
+      .toISOString()
+      .split('T')[0];
 
     this.pomoList.forEach((task: any) => {
-      const date = new Date(task.endDateTime).toISOString().split('T')[0];
-      if (todayDate == date) {
+      const dateStr = task.endDateTime;
+      const dateParts = dateStr.split(' ');
+      const date = dateParts[0].split('-');
+      const time = dateParts[1].split(':');
+
+      const parsedDate = new Date(
+        Date.UTC(
+          parseInt(date[2], 10),
+          parseInt(date[1], 10) - 1,
+          parseInt(date[0], 10),
+          parseInt(time[0], 10),
+          parseInt(time[1], 10),
+          parseInt(time[2], 10)
+        )
+      );
+
+      const formattedDate = parsedDate.toISOString().split('T')[0];
+
+      if (todayDate == formattedDate) {
         this.productiveTime += task.minutesTaken;
       }
+
       if (task.endDateTime) {
-        const date = new Date(task.endDateTime).toISOString().split('T')[0];
-        if (!pomoSessByDate[date]) {
-          pomoSessByDate[date] = 0;
+        if (!pomoSessByDate[formattedDate]) {
+          pomoSessByDate[formattedDate] = 0;
         }
-        pomoSessByDate[date] += task.minutesTaken;
+        pomoSessByDate[formattedDate] += task.minutesTaken;
       }
     });
 
@@ -353,7 +386,7 @@ export class DashboardComponent {
       .subscribe(
         async (res: any) => {
           this.pomoList = res;
-          this.eachTaskStackedChart();
+
           this.totalPomoSessBarChart();
         },
         error => {
