720d5f1af1ac2fd581f74482d50c6ac7be59a36c
scoopydoooKing <148352740+scoopydoooKing@users.noreply.github.com>
2024-07-01 01:06:31 +0800

    toast for pomo

--- FE/src/app/components/pomodoro/pomodoro.component.ts
+++ FE/src/app/components/pomodoro/pomodoro.component.ts
@@ -236,9 +236,14 @@ export class PomodoroComponent implements OnInit {
 
   }
   decrease() {
-    if (!this.isRunning && this.mins > 0) {
+    if (!this.isRunning && this.mins > 1) {
       this.mins -= 1;
       this.initialMins -= 1;
+    }
+    else {
+      this.toastr.error("Each session has to be at least 1 minute", 'Error', {
+        positionClass: 'toast-top-center',
+      });
     }
 
   }
