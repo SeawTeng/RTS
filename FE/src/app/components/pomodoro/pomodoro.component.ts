60f8cdf4aefd073a86e2a804bf832466a9afaa0b
scoopydoooKing <148352740+scoopydoooKing@users.noreply.github.com>
2024-07-01 02:52:55 +0800

    changes to dashboard to update Task Overview (completed Today)

--- FE/src/app/components/pomodoro/pomodoro.component.ts
+++ FE/src/app/components/pomodoro/pomodoro.component.ts
@@ -1,53 +1,211 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import Chart from 'chart.js/auto';
+import { HttpClientModule } from '@angular/common/http';
 import { ServicesService } from '../../services/services.service';
-import { ToDoCategoryComponent } from '../to-do/to-do-category-model/to-do-category-model.component';
-import { ToDoTaskComponent } from '../to-do/to-do-task-model/to-do-task-model.component';
-import { ToDoComponent } from '../to-do/to-do.component';
 import { ToastrService } from 'ngx-toastr';
-import Swal from 'sweetalert2';
-import moment from 'moment';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { start } from 'repl';
 import { NgxLoadingModule } from 'ngx-loading';
 
+
 @Component({
-  selector: 'app-pomodoro',
+  selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule, ToDoCategoryComponent,
-    ToDoTaskComponent, NgxLoadingModule],
-  templateUrl: './pomodoro.component.html',
-  styleUrl: './pomodoro.component.scss',
+  imports: [CommonModule, FormsModule, NgxLoadingModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
 })
-export class PomodoroComponent implements OnInit {
-  initialMins = 25;
-  mins = 25;
-  secs = 0;
-  isRunning = false;
-  isBreak = false;
-  initialBreak = 5;
-  breakMins = 5;
-  breakSecs = 0
-
-  isPomoPage = true;
-  isBreakPage = false;
+export class DashboardComponent {
 
 
+  //charts 
+  title = 'ng-chart';
+  cTaskchart: any = null;
+  TaskStatuschart: any = null;
+  eachTaskStackChart: any = [];
+  pomoBar: any = null;
+
+
+  //get methods of to do and pomo 
   categoryList: any = [];
   taskList: any = [];
   pomoList: any = [];
   loading = false;
-  type = 'Create';
   selectedCategoryId: any = null;
   selectedTask: any = null;
-  selectedCategory: any = null;
   showCompleted: boolean = true;
+
+  // upper dashboard 
+  dueToday = 0;
+  completedToday = 0;
+  overdueTasks = 0;
+  productiveTime = 0;
+
+  //filter 
+  selectedPeriod: number = 7;
+  selectedCategory: any = null;
+  selectedPeriodforPomo: number = 7;
 
 
   constructor(
     private service: ServicesService,
     private toastr: ToastrService,
-    public router: Router
+
   ) { }
+
+  async ngOnInit() {
+    await this.getAllCategory();
+    await this.getAllPomoSess();
+
+  }
+
+  // _____       ____           ____ _                _       
+  // |_   _|__   |  _ \  ___    / ___| |__   __ _ _ __| |_ ___ 
+  //   | |/ _ \  | | | |/ _ \  | |   | '_ \ / _` | '__| __/ __|
+  //   | | (_) | | |_| | (_) | | |___| | | | (_| | |  | |_\__ \
+  //   |_|\___/  |____/ \___/   \____|_| |_|\__,_|_|   \__|___/
+
+  async numOfCompletedTaskBarChart(period?: number) {
+    const today = new Date();
+    const SgDay = new Date(today);
+    SgDay.setDate(SgDay.getDate() + 1);
+
+    let startDate: Date;
+    const periodNum = Number(period)
+
+    switch (periodNum) {
+      case 7:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        break;
+      case 31:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 31);
+        break;
+      case 93:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 93);
+        break;
+      default:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        break;
+    }
+    const sgdayString = SgDay.toISOString().split('T')[0];
+
+
+    const filteredTasks = this.taskList.filter((task: any) => {
+      const taskDate = new Date(task.endDate);
+      const isCompleted = task.status === 'completed';
+      const isInRange = taskDate >= startDate && taskDate <= today;
+      return isInRange && isCompleted;
+    });
+
+    const filteredTasksByDate: { [date: string]: number } = {};
+
+    filteredTasks.forEach((task: any) => {
+      const date = new Date(task.endDate).toISOString().split('T')[0];
+      if (!filteredTasksByDate[date]) {
+        filteredTasksByDate[date] = 0;
+      }
+      filteredTasksByDate[date]++;
+
+      if (date === sgdayString) {
+        this.completedToday += 1;
+      }
+    });
+
+    const sortedEntries = Object.entries(filteredTasksByDate).sort((a, b) => {
+      return new Date(a[0]).getTime() - new Date(b[0]).getTime();
+    });
+
+    const labels = sortedEntries.map(entry => entry[0]);
+    const data = sortedEntries.map(entry => entry[1]);
+
+    if (this.cTaskchart) {
+      this.cTaskchart.data.labels = labels;
+      this.cTaskchart.data.datasets[0].data = data;
+      this.cTaskchart.update();
+    } else {
+      this.cTaskchart = new Chart('completedTaskBarChart', {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: '# of Completed Tasks',
+              data: data,
+              borderWidth: 1,
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgba(75, 192, 192, 1)',
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    }
+  }
+
+  async updateCompletedTaskChart() {
+    await this.numOfCompletedTaskBarChart(this.selectedPeriod);
+  }
+
+  async updateCategory(selectedCategories: string) {
+    this.taskStatusPieChart(selectedCategories);
+  }
+
+  async taskStatusPieChart(selectedCategories?: string) {
+    const filteredTasks = this.taskList.filter((task: any) => {
+      const categoryId = task.categoryId._path.segments[1];
+      return selectedCategories ? categoryId === selectedCategories : true;
+    });
+
+    const pendingTasks = filteredTasks.filter((task: any) => task.status === 'active').length;
+    const completedTasks = filteredTasks.filter((task: any) => task.status === 'completed').length;
+
+    if (this.TaskStatuschart) {
+      this.TaskStatuschart.data.datasets[0].data = [pendingTasks, completedTasks];
+      this.TaskStatuschart.update();
+    } else {
+      this.TaskStatuschart = new Chart('taskstatuspiechart', {
+        type: 'pie',
+        data: {
+          labels: ['Pending Tasks', 'Completed Tasks'],
+          datasets: [
+            {
+              label: 'Task Status',
+              data: [pendingTasks, completedTasks],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
+          },
+        },
+      });
+    }
+  }
+
+
+
 
   //   _                 _         _            _
   //  | |               | |       | |          | |
@@ -56,14 +214,6 @@ export class PomodoroComponent implements OnInit {
   //  | || (_) |    | (_| | (_) | | || (_| \__ \   <
   //   \__\___/      \__,_|\___/   \__\__,_|___/_|\_\
 
-  async ngOnInit() {
-    await this.getAllCategory();
-    await this.getAllPomoSess();
-  }
-
-  chooseTask(task: any) {
-    this.selectedTask = task;
-  }
 
   async getAllCategory() {
     this.loading = true;
@@ -80,13 +230,18 @@ export class PomodoroComponent implements OnInit {
             .httpCall(
               api,
               {
-
+                showCompleted: this.showCompleted,
               },
               'post'
             )
             .subscribe((res: any) => {
               this.loading = false;
               this.taskList = res;
+
+
+              this.numOfCompletedTaskBarChart();
+              this.taskStatusPieChart();
+              this.findOverdueTask();
             });
         },
         error => {
@@ -98,37 +253,152 @@ export class PomodoroComponent implements OnInit {
       );
   }
 
+  async findOverdueTask() {
+    const today = new Date(new Date().getTime() + 8 * 60 * 60 * 1000)
+    const todayDate = today
+      .toISOString()
+      .split('T')[0];
+    const pendingTasks = this.taskList.filter((task: any) => task.status === 'active');
+    this.dueToday = 0;
+    this.overdueTasks = 0;
 
-  async markTaskAsComplete(task: any) {
-    const data = {
-      status: 'completed',
-    };
+    pendingTasks.forEach((task: any) => {
 
-    const { value: confirm } = await Swal.fire({
-      title: 'Complete Task',
-      text: 'Do you sure you want to mark this task as completed?',
-      showCancelButton: true,
+      const taskEndDate = new Date(task.endDate);
+      const taskEndDateString = taskEndDate.toISOString().split('T')[0];
+
+      if (taskEndDateString == todayDate) {
+        this.dueToday += 1;
+      } else if (taskEndDate < today) {
+        this.overdueTasks += 1;
+      }
+    });
+  }
+
+
+  // ____                          ____ _                _       
+  // |  _ \ ___  _ __ ___   ___    / ___| |__   __ _ _ __| |_ ___ 
+  // | |_) / _ \| '_ ` _ \ / _ \  | |   | '_ \ / _` | '__| __/ __|
+  // |  __/ (_) | | | | | | (_) | | |___| | | | (_| | |  | |_\__ \
+  // |_|   \___/|_| |_| |_|\___/   \____|_| |_|\__,_|_|   \__|___/
+
+
+
+  async totalPomoSessBarChart(period?: number) {
+
+
+    const today = new Date();
+    const SgDay = new Date(today);
+    SgDay.setDate(SgDay.getDate() + 1);
+
+    let startDate: Date;
+    const periodNum = Number(period);
+
+    switch (periodNum) {
+      case 7:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        break;
+      case 31:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 31);
+        break;
+      case 93:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 93);
+        break;
+      default:
+        startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        break;
+    }
+
+    const pomoSessByDate: { [date: string]: number } = {};
+    const todayDate = new Date(new Date().getTime() + 8 * 60 * 60 * 1000)
+      .toISOString()
+      .split('T')[0];
+
+    this.pomoList.forEach((task: any) => {
+      const dateStr = task.endDateTime;
+      const dateParts = dateStr.split(' ');
+      const date = dateParts[0].split('-');
+      const time = dateParts[1].split(':');
+
+      const parsedDate = new Date(
+        Date.UTC(
+          parseInt(date[2], 10),
+          parseInt(date[1], 10) - 1,
+          parseInt(date[0], 10),
+          parseInt(time[0], 10),
+          parseInt(time[1], 10),
+          parseInt(time[2], 10)
+        )
+      );
+
+      const formattedDate = parsedDate.toISOString().split('T')[0];
+
+      // For the upper dashboard 
+      if (todayDate == formattedDate) {
+        this.productiveTime += task.minutesTaken;
+      }
+
+
+      if (task.endDateTime) {
+        if (!pomoSessByDate[formattedDate]) {
+          pomoSessByDate[formattedDate] = 0;
+        }
+        pomoSessByDate[formattedDate] += task.minutesTaken;
+      }
     });
 
-    if (confirm) {
-      this.loading = true;
-      await this.service
-        .httpCall(this.service.updateTodoTask(task.id), data, 'put')
-        .subscribe(
-          async (res: any) => {
-            await this.getAllCategory();
-          },
-          error => {
-            this.loading = false;
-            this.toastr.error(error.error, 'Error', {
-              positionClass: 'toast-top-center',
-            });
-          }
-        );
+
+    const filteredDates = Object.keys(pomoSessByDate).filter(date => {
+      const dateObj = new Date(date);
+      return dateObj >= startDate && dateObj <= SgDay;
+    });
+
+
+    filteredDates.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+
+
+    const filteredData = filteredDates.map(date => pomoSessByDate[date]);
+
+    const labels = filteredDates;
+    const data = filteredData;
+
+    if (this.pomoBar) {
+      this.pomoBar.data.labels = labels;
+      this.pomoBar.data.datasets[0].data = data;
+      this.pomoBar.update();
     } else {
-      this.loading = false;
+
+      this.pomoBar = new Chart('pomoBarChart', {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: 'Total Time Spent Each Day ',
+              data: data,
+              borderWidth: 1,
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgba(75, 192, 192, 1)',
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
     }
   }
+
+  async updatePomoBarChart() {
+    await this.totalPomoSessBarChart(this.selectedPeriodforPomo);
+
+  }
+
 
   // ____                           _                 
   // |  _ \ ___  _ __ ___   ___   __| | ___  _ __ ___  
@@ -138,45 +408,16 @@ export class PomodoroComponent implements OnInit {
   // |_   _(_)_ __ ___   ___ _ __                      
   //   | | | | '_ ` _ \ / _ \ '__|                     
   //   | | | | | | | | |  __/ |                        
-  //   |_| |_|_| |_| |_|\___|_|            
-
-
-  async createPomoSess() {
-    const endDateTime = new Date();
-    const endDate = moment(endDateTime).format("DD-MM-YYYY HH:mm:ss")
-    const startDate = new Date(endDateTime.getTime() - this.initialMins * 60 * 1000)
-    const data = {
-      breakSession: false,
-      minutesTaken: this.initialMins,
-      secondsTaken: 0,
-      pomodoroSession: true,
-      taskName: this.selectedTask.title,
-      endDateTime: endDate,
-      startDateTime: moment(startDate).format("DD-MM-YYYY HH:mm:ss")
-    }
-
-    const api = this.service.createPomoSess();
-    const type = 'post';
-
-    await this.service.httpCall(api, data, type).subscribe(
-      () => { },
-      error => {
-        this.loading = false;
-        this.toastr.error(error.error, 'Error', {
-          positionClass: 'toast-top-center',
-        });
-      }
-    );
-  }
-
+  //   |_| |_|_| |_| |_|\___|_|       
 
   async getAllPomoSess() {
-
     await this.service
       .httpCall(this.service.getAllPomoSess(), {}, 'get')
       .subscribe(
         async (res: any) => {
           this.pomoList = res;
+
+          this.totalPomoSessBarChart();
         },
         error => {
           this.toastr.error(error.error, 'Error', {
@@ -185,141 +426,6 @@ export class PomodoroComponent implements OnInit {
         }
       );
   }
-
-  pomoPage() {
-    this.isPomoPage = true;
-    this.isBreakPage = false;
-  }
-
-  breakPage() {
-    this.isPomoPage = false;
-    this.isBreakPage = true;
-  }
-
-  run() {
-    if (this.selectedTask != null) {
-      if (this.initialMins > 0) {
-        this.isRunning = true;
-        this.counter();
-        this.isBreak = false;
-      }
-      else {
-        this.toastr.error("Each session has to be at least 1 minute", 'Error', {
-          positionClass: 'toast-top-center',
-        });
-      }
-    }
-    else {
-      this.toastr.error("Select a task you want to work on", 'Error', {
-        positionClass: 'toast-top-center',
-      });
-    }
-
-
-  }
-
-  reset() {
-    this.mins = 25;
-    this.secs = 0;
-    this.isRunning = false;
-  }
-
-  pause() {
-    this.isRunning = false;
-  }
-  increase() {
-    if (!this.isRunning) {
-      this.mins += 1;
-      this.initialMins += 1;
-
-    }
-
-  }
-  decrease() {
-    if (!this.isRunning && this.mins > 1 && this.initialMins > 1) {
-      this.mins -= 1;
-      this.initialMins -= 1;
-    }
-    else {
-      this.toastr.error("Each session has to be at least 1 minute", 'Error', {
-        positionClass: 'toast-top-center',
-      });
-    }
-
-  }
-  increaseBreak() {
-    if (!this.isRunning)
-      this.breakMins += 1;
-  }
-  decreaseBreak() {
-    if (!this.isRunning && this.breakMins > 0)
-      this.breakMins -= 1;
-  }
-  breakTime() {
-    this.counterBreak();
-  }
-
-  playAudio() {
-    let audio = new Audio();
-    audio.src = "../assets/bedside-clock-alarm-95792.mp3";
-    audio.load();
-    audio.play();
-
-  }
-
-  counter() {
-    setTimeout(() => {
-      if ((this.mins || this.secs) && this.isRunning) {
-        if (this.secs === 0) {
-          this.secs = 59;
-          this.mins -= 1;
-
-        } else {
-          this.secs -= 1;
-
-        }
-        this.counter();
-      };
-      if (this.mins == 0 && this.secs == 0) {
-        //play sound && add to db 
-        this.playAudio();
-        this.breakPage();
-        this.breakTime();
-        this.isRunning = false;
-        this.isBreak = true;
-
-        this.createPomoSess();
-        this.markTaskAsComplete(this.selectedTask);
-        this.reset();
-      };
-
-    }, 1000);
-  }
-  counterBreak() {
-    setTimeout(() => {
-      if ((this.breakMins || this.breakSecs) && this.isBreak) {
-        if (this.breakSecs === 0) {
-          this.breakSecs = 59;
-          this.breakMins -= 1;
-
-        } else {
-          this.breakSecs -= 1;
-
-        }
-        this.counterBreak();
-      };
-      if (this.breakMins == 0 && this.breakSecs == 0) {
-        this.counter();
-        this.isRunning = true;
-        this.isBreak = false;
-        this.pomoPage();
-        this.reset();
-      };
-
-    }, 1000);
-  }
-
-
 
 
 }
